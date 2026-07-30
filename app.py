a777b57cfe053012f3f40fb78dafed0d14e6904a
SaluAnnGeorge <161289769+SaluAnnGeorge@users.noreply.github.com>
2026-07-30 23:41:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # Configure the Gemini API
-genai.configure(api_key=("GEMINI_API_KEY"))
+genai.configure(api_key=(""GEMINI_API_KEY""))
 
 # Set up the generation configuration
 generation_config = {
